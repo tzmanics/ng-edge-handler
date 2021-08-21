f7b4dbc8c597f13323f202b6033e85abe25bb28f
Tara Z. Manicsic <tzmanics@gmail.com>
2021-08-20 22:38:46 -0400

    removes template filler

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,3 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-edge-handler');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-edge-handler app is running!');
-  });
-});
